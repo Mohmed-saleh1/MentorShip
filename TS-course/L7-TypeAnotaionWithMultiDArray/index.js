c1e9b048d118e03ce640443349a29eda48c30a52
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-10 21:23:08 +0200

    Annotaions with Multidimensional Arrays

---
+++ TS-course/L7-TypeAnotaionWithMultiDArray/index.js
@@ -0,0 +1,5 @@
+////////////  Annotaions with Multidimensional Arrays  //////////
+var arrayOne = [1, 2, 3, 4, 5];
+var arrayTwo = ['a', 'b', 'c'];
+var arrayThree = [1, 2, 3, 4, 5, "A", "B", "C"];
+var arrayFour = [1, 2, 3, 4, 5, "A", "B", ["C", "D"], true, false]; // we can define the multidimentional array data type by this way 
---
+++ TS-course/L7-TypeAnotaionWithMultiDArray/index.ts
@@ -0,0 +1,6 @@
+////////////  Annotaions with Multidimensional Arrays  //////////
+
+let arrayOne: number[]=[1,2,3,4,5];
+let arrayTwo: string[]=['a','b','c'];
+let arrayThree: (number|string)[]=[1,2,3,4,5,"A","B","C"];
+let arrayFour: (number|string|string[]|boolean)[]=[1,2,3,4,5,"A","B",["C","D"],true,false];  // we can define the multidimentional array data type by this way 
