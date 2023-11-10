ff1a19bec2a5fd359bd51f1a07b157668cf2adee
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-10 18:06:30 +0200

    lesson 4

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// let num = 10;
+// num = "Muhamed"
+// console.log(num); 
+////// => we can't reassign a variable with different data type /////////
+
+// let age = 40;
+// if(age > 30){
+//     console.log("Good");    
+// }else{
+//     console.log(age.repeate(3));
+// }
+/////// => TS can recognize the errors in compiletime envieronment  //////////
+
+
+
+
