03d04c1e8dc096fe7c6aa4cc1926626d439aa5cd
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-11 16:35:09 +0200

    Rest Parameter

--- TS-course/L10-RestParameter/index.ts
+++ TS-course/L10-RestParameter/index.ts
@@ -1,17 +1,16 @@
 /**
  * Function
  * Rest Parameter
- * what about float => it relates to the datatype 'number' ..
+ * what about float => it relates to the datatype number
  */
 
-function addAll(...nums:number[]) {
-    let sum = 0;
-    // for (let i = 0; i < nums.length; i++) {
-    //     return sum+nums[i]       
-    // }
-    nums.forEach((num)=>{
-        return sum + num;
-    })
+function addAll(...nums:number[]): number {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+         result+=nums[i];
+            }
+    // nums.forEach((num)=> result+=num)
+    return result;
 }
 
-console.log(addAll(10,10,10,10));
+console.log(addAll(10,10,10,10.5,+true));
