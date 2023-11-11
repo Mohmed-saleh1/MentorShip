e67f9092dd2b3230304ad9a579ac559d2abcb000
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-11 16:51:45 +0200

    type alias

---
+++ TS-course/L12-DataTypes-TypesAlias/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Data Types
+ * ..Type Alias => we can combine more than data type together 
+ */
+
+type st = string;
+let theName :st="Muhamed"
+
+type num = number;
+ let theNumber : num = 20;
+
+ type stAndNum = string|number // => the same with  type stAndNum = st|num
+ let alll:stAndNum="ahmed"
+  alll= 9;
