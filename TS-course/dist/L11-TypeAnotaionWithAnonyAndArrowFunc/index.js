ed41cefca369ab7bc3788b2c6036e8a40b67b72c
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-12 01:13:44 +0200

    tsconfig.json modifying

---
+++ TS-course/dist/L11-TypeAnotaionWithAnonyAndArrowFunc/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const addWithAnony = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(addWithAnony(4, 5));
+const addWithArrow = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(addWithArrow(4, 5));
+//# sourceMappingURL=index.js.map
---
+++ TS-course/src/L9-optionalAndDefaultParameter/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function showMeData(name = 'un', age, salary) {
+    return ` ${name} - ${age} - ${salary}`;
+}
+console.log(showMeData(undefined, 22));
+//# sourceMappingURL=index.js.map
