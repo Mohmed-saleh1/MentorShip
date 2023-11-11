cdf1b9cc36d33ed7168f5894f38291781e6bbb35
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-12 00:47:51 +0200

    enums

---
+++ TS-course/L17-Enums/index.ts
@@ -0,0 +1,19 @@
+
+
+const KIDS = 15;
+const EASY = 9;
+const MEDIUM = 6;
+const HARD = 3;
+
+enum level {
+    kids = 15,
+    Easy = 9,
+    medium = 6,
+    hard = 3
+}
+
+let lvl : string = "Easy" ;
+if (lvl==="Easy") {
+    console.log(`the level Is ${lvl} and Number of seconds is ${level.Easy}`);
+    
+}
