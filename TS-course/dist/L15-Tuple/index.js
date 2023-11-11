6194f824ef11d09fbce9b340aaef3cbef82cb594
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2023-11-12 01:16:48 +0200

    Delete TS-course/dist directory

--- TS-course/dist/L15-Tuple/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let articles = [22, "Title-one", false];
-articles = [11, "title-two", true];
-console.log(articles);
-const [id, title, published] = articles;
-//# sourceMappingURL=index.js.map
--- TS-course/dist/L9-optionalAndDefaultParameter/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function showMeData(name = 'un', age, salary) {
-    return ` ${name} - ${age} - ${salary}`;
-}
-console.log(showMeData(undefined, 22));
-//# sourceMappingURL=index.js.map
