02553ba03fbb2cff1b5d96590de0fd8a1fbb1505
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-11 12:43:16 +0200

    Optional and Default Parameter

--- TS-course/L9-optionalAndDefaultParameter/index.ts
+++ TS-course/L9-optionalAndDefaultParameter/index.ts
@@ -1,6 +1,7 @@
-/**
- * Function
- * -optional and Default Parameters
- * 
- * In Js , Every Para
- */
+
+
+function showMeData(name: string='un',age: number,salary?: string){ // the "?" make the parameter optional
+    return ` ${name} - ${age} - ${salary}`
+}
+console.log(showMeData(undefined,22));
+ 
---
+++ TS-course/L9-optionalAndDefaultParameter/test.js
@@ -0,0 +1,6 @@
+
+
+function showData(name='un',age,country){
+    return ` ${name} - ${age} - ${country}`
+}
+console.log(showData(undefined,22,'egypt'));
