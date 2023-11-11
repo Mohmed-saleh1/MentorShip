57c8a9637ea8268d90b9807dffdda52fb91044ec
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-11 16:42:44 +0200

    anonymous and arrow function

---
+++ TS-course/L11-TypeAnotaionWithAnonyAndArrowFunc/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Function 
+ * .. Anonymous function
+ * .. arrow function
+ */
+
+const addWithAnony = function(num1:number,num2:number):number{
+    return num1+num2;
+}
+console.log(addWithAnony(4,5));
+
+const addWithArrow = (num1:number,num2:number):number =>{
+    return num1+num2
+}
+
+console.log(addWithArrow(4,5));
+
