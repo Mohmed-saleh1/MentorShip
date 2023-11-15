e6968e587c34874ff98cd2a9527ce9fca66fd952
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-15 18:00:55 +0200

    interface reopen

---
+++ TS-course/src/L24-InterfaceReopen/index.ts
@@ -0,0 +1,26 @@
+ 
+
+//Homepage
+ interface settings {
+  them:boolean,
+  font:string,
+ }
+
+//Article page
+interface settings {
+  sidebar:boolean;
+}
+
+//contact page 
+
+interface settings {
+  external :boolean
+}
+
+
+let userSettings :settings={
+  them:true,
+  font:'sans-serf',
+  sidebar:false,
+  external:true
+}
