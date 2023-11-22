15cddfb349ba388f19945b13f3b64848299c6110
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-22 17:39:57 +0200

    adding server file

---
+++ NodeJs-Task-1/server.ts
@@ -0,0 +1,25 @@
+// app.ts
+
+import express from 'express';
+import routes from './routes/authRoutes';
+import dotenv from 'dotenv';
+dotenv.config();
+import dbconnection from './config/dbConfig'
+
+
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Database connection
+dbconnection();
+
+// Mount the routes at a specific path
+app.use('/api/users', routes);
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
