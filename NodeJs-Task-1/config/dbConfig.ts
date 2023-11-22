1973388b6ff21cb651cbedfd6a565711b08012e6
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-11-22 17:35:06 +0200

    adding the db configurations

---
+++ NodeJs-Task-1/config/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const MONGO_URL = 'mongodb+srv://musaleh:musaleh@cluster0.j1uipq8.mongodb.net/?retryWrites=true&w=majority'
+
+const connectDB = async () => {
+  try {
+    if (!MONGO_URL) {
+      throw new Error('MongoDB URI is not provided');
+    }
+
+    await mongoose.connect(MONGO_URL);
+
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit the process if unable to connect to the database
+  }
+};
+
+export default connectDB;
